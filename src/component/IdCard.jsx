007dd571e4b80f681f4ab189d61fd9f89fdb19cd
Mohammed Irfan <irfan.arshad4@gmail.com>
2022-10-14 14:59:45 +0530

    Initial commit for PR

---
+++ src/component/IdCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const IcCard = ({lastName, firstName, gender, height, birth, picture }) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default IcCard;
