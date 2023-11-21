120b29822680698c381ad48e43610aeac176eff0
Andivyy11 <andivyy@gmail.com>
2023-11-21 10:53:07 +0530

    solved lab

--- src/components/CreditCard.js
+++ src/components/CreditCard.js
@@ -1,7 +1,9 @@
 import './CreditCard.css'
+import visa from '../assets/images/visa.png'
+import master from '../assets/images/mastercard.png'
 function CreditCard(props)
 {
-    // const hrf = props.type === 'visa' ? visaImage : masterCard;
+    const img = props.type === 'Visa' ? visa : master;
     let mon=props.expirationMonth
     let yr=props.expirationYear%100
     let ex=''
@@ -9,7 +11,9 @@ function CreditCard(props)
       ex='0'+mon+'/'+yr
     console.log(ex)
     return <div className='credit' style={{backgroundColor: props.bgColor,color:props.color}}>
-        <img src='./assets/images/visa.png' alt='pic' />
+    <div>
+        <img src={img} alt='pic' />
+        </div>
         <h1>.... .... .... {props.number.slice(12)}</h1>
         <p>Expires {ex} {props.bank}</p>
         <p>{props.owner}</p>
