2b64d715999e44cdec99e3c45e684459918b246d
Andivyy11 <andivyy@gmail.com>
2023-11-21 10:32:44 +0530

    Solved lab

---
+++ src/components/SingleColor.js
@@ -0,0 +1,10 @@
+
+function SingleColor(props)
+{
+    return <div>
+        <label>{(props.color).toUpperCase()} : </label>
+        <input type='number' min={0} max={255} value={props.value} onChange={(event) => props.onChange(parseInt(event.target.value, 10))} />
+    </div>
+}
+
+export default SingleColor
