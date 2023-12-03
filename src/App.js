3bbeb482cbeb760a78efec36036fa7e23bf90b5f
Official-Lovely-kumari <officiallovelykumari@gmail.com>
2023-12-03 22:17:51 +0530

    done

--- src/App.js
+++ src/App.js
@@ -22,4 +22,50 @@ function App() {
   );
 }
 
+//export default App;
+import React from 'react';
+import IdCard from './components/idCard';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Other components or content goes here */}
+      
+      {/* Render the IdCard component */}
+      <IdCard
+        lastName='Doe'
+        firstName='John'
+        gender='male'
+        height={178}
+        birth={new Date("1992-07-14")}
+        picture="https://randomuser.me/api/portraits/men/44.jpg"
+      />
+      <IdCard
+        lastName='Delores'
+        firstName='Obrien'
+        gender='female'
+        height={172}
+        birth={new Date("1988-05-11")}
+        picture="https://randomuser.me/api/portraits/women/44.jpg"
+      />
+    </div>
+  );
+}
+
 export default App;
+import React from 'react';
+import Greetings from './components/Greetings';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Other components or content goes here */}
+      
+      {/* Render the Greetings component */}
+      <Greetings lang="de">Ludwig</Greetings>
+      <Greetings lang="fr">François</Greetings>
+    </div>
+  );
+}
+
+// export default App;
