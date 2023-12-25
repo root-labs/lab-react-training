4f154f81759bfb8756bcfa7e58caedec0f2cb6e2
Vasanth Kumar <avkhero4pc@gmail.com>
2023-12-25 14:37:02 +0530

    Vasanth Kumar-Internship

---
+++ components/BoxColor.js
@@ -0,0 +1,14 @@
+function BoxColor(props)
+{
+    const toHex = (number) => {
+        let hex = number.toString(16);
+        return hex.length === 1 ? "0" + hex : hex;
+    };
+    let color='#'+(toHex(props.r))+(toHex(props.g))+(toHex(props.b))
+    return <div className='cbox' style={{backgroundColor:color}}>
+          <p>rgb({props.r},{props.g},{props.b})</p>
+          <p>{color}</p>
+    </div>
+}
+
+export default BoxColor
