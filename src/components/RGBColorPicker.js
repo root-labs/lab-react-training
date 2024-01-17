67d5284bc5694976bb7ad5235b1265fcf3ae5f50
Nhithees M <98444584+NhitheesM@users.noreply.github.com>
2024-01-17 12:39:47 +0530

    done

---
+++ src/components/RGBColorPicker.js
@@ -0,0 +1,53 @@
+import SingleColor from "./SingleColor"
+import React from 'react';
+
+function RGBColorPicker()
+{
+	const bxStyle={
+		width:'50px',
+		height:'50px',
+		display:'inline-block',
+		border:'1px solid black',
+	}
+	const container={
+		display:'flex',
+		width:'200px',
+		padding:'15px 20px',
+		alignItems:'center',
+	}
+	const [rValue, setRValue]=React.useState(255) 
+	const [gValue, setGValue]=React.useState(150)
+	const [bValue, setBValue]=React.useState(0)
+  return <div> 
+  <div style={container}>
+    <div style={{...bxStyle ,backgroundColor:'rgb('+rValue+',0,0)'}}></div>
+    <SingleColor
+	  color="r"
+	  value={rValue}
+	  onChange={ (value) => { setRValue(value) } }
+    />
+ </div>
+ <div style={container}>
+	<div style={{...bxStyle ,backgroundColor:'rgb(0,'+gValue+',0)'}}></div>
+	<SingleColor
+	  color="g"
+	  value={gValue}
+	  onChange={ (value) => { setGValue(value) } }
+    />
+ </div>
+ <div style={container}>
+	<div style={{...bxStyle ,backgroundColor:'rgb(0,0,'+bValue+')'}}></div>
+	<SingleColor
+	  color="b"
+	  value={bValue}
+	  onChange={ (value) => { setBValue(value) } }
+    />
+ </div>
+ <div style={container}>
+	<div style={{...bxStyle, backgroundColor:'rgb('+rValue+','+gValue+','+bValue+')'}}></div>
+	<span>rgb({rValue},{gValue},{bValue})</span>
+ </div>
+</div>
+}
+
+export default RGBColorPicker
