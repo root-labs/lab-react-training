daed8356fa061749bd727161fecdcb0e082f0960
Mohammad Adnan <111442736+Mohammadadnan07@users.noreply.github.com>
2024-01-31 12:00:30 +0530

    Update Rating.js

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,1 +1,21 @@
-
+function Rating(props)
+{
+    let rate=Math.round(props.children)
+    let s=''
+    let i=1
+    while(i<=rate)
+    {
+        s+='★'
+        ++i;
+    }
+    while(i<=5)
+    {
+        s+='☆'
+        i++;
+    }
+    return <div>
+        <p style={{fontSize : '20px'}}>{s}</p>
+        <p style={{fontSize : '50px'}}>{s}</p>
+    </div>
+}
+export default Rating
