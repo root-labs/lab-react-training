f964aace1a152fbb7a1c7d0baaeac84bdc3b13d9
Mohammad Adnan <111442736+Mohammadadnan07@users.noreply.github.com>
2024-01-31 11:59:12 +0530

    Update likebutton.js

--- src/components/likebutton.js
+++ src/components/likebutton.js
@@ -1,1 +1,15 @@
+import './LikeButton.css'
+import React from 'react';
 
+let colors=['purple','blue','green','yellow','orange','red']
+function LikeButton(props)
+{
+   const [likes,setlikes]=React.useState(0)
+   function tap()
+   {
+       setlikes(likes+1)
+   }
+   return <button className='btn' style={{backgroundColor: colors[likes%6]}} onClick={tap}>{likes} Likes</button>
+}
+
+export default LikeButton
