1cb8bc1446d0480ff615af1a7125cb64afed2d74
Mohammad Adnan <111442736+Mohammadadnan07@users.noreply.github.com>
2024-01-31 12:01:17 +0530

    Update Table.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,1 +1,17 @@
-
+import './Table.css'
+function Table(props)
+{
+    const divs=[]
+    let i=1;
+    while(i<=props.limit)
+    {
+        if(i%2===0)
+        divs.push(<div className="even">{i}</div>)
+        else 
+        divs.push(<div className="odd">{i}</div>)
+        ++i;
+    }
+    console.log(divs)
+      return <div>{divs}</div>;
+}
+export default Table
