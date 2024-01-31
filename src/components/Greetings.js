9c5cf766e8b3435d276e82715b7d0ea9ff740d78
Mohammad Adnan <111442736+Mohammadadnan07@users.noreply.github.com>
2024-01-31 11:58:14 +0530

    Update Greetings.js

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,1 +1,24 @@
+import './Greeting.css'
+
+function Greeting(props)
+{
+    let sayHello=''
+    switch (props.lang) {
+        case 'de':
+          sayHello= `Hallo`;
+          break;
+        case 'fr':
+          sayHello = `Bonjour`;
+          break;
+        default:
+          sayHello = `Hello`;
+      }
+    return (
+       <div className='greet'>
+           <span>{sayHello} {props.children}!</span>
+       </div>
+    );
+}
+
+export default Greeting
 
