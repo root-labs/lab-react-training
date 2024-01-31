19cc5d95de889c5e945d99761b51c8c754dd254c
Mohammad Adnan <111442736+Mohammadadnan07@users.noreply.github.com>
2024-01-31 11:52:42 +0530

    Update Carousel.js

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,1 +1,27 @@
+import React from 'react';
+import './Caraousel.css'
 
+function Caraousel(props)
+{
+    let arr=props.images
+    let len=arr.length
+    const[img_num , setImg]=React.useState(2)
+    function left()
+    {
+        if(img_num===0)
+        setImg(len-1)
+        else
+        setImg((img_num-1)%len)
+    }
+    function right()
+    {
+        setImg((img_num+1)%len)
+    }
+    return <div className='caraousel'>
+        <button onClick={left}>Left</button>
+        <img src={arr[img_num]} alt='pic' style={{width:'300px'}}/>
+        <button onClick={right}>Right</button>
+    </div>
+}
+
+export default Caraousel
