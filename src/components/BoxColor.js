7a5ef992eb725a81e98a59e563e85035a540f0f5
harvey <harveyreginaldspectre1@gmail.com>
2024-02-03 10:14:01 +0530

    done

---
+++ src/components/BoxColor.js
@@ -0,0 +1,16 @@
+import './BoxColor.css'
+
+function BoxColor(props)
+{
+    const toHex = (number) => {
+        let hex = number.toString(16);
+        return hex.length === 1 ? "0" + hex : hex;
+    };
+    let color='#'+(toHex(props.r))+(toHex(props.g))+(toHex(props.b))
+    return <div className='cbox' style={{backgroundColor:color}}>
+          <p>rgb({props.r},{props.g},{props.b})</p>
+          <p>{color}</p>
+    </div>
+}
+
+export default BoxColor
