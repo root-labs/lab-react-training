04e5097d232de359ecaa96f267853affa44fb38a
sai preetham ravula <160527233+VSPR1@users.noreply.github.com>
2024-06-17 23:26:51 +0530

    Create CC.js

---
+++ src/Components/CC.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const CreditCard = (props) => {
+    const num = 'XXXX XXXX XXXX '
+    const divstyle ={
+        backgroundColor:`${props.bgColor}`,
+        color:`${props.color}`,
+        display:'flex',
+        flexDirection: 'column',
+        borderRadius: '10px',
+        justifyContent: 'space-around',
+        width:'300px',
+        height:'130px',
+        alignItems:'center'
+
+
+    }
+  return (
+    <div className='card' style={divstyle} >
+        <b style={{marginLeft:'250px',color:'blue'}}>{props.type}</b>
+       <div>{num}{props.number.substr(-4)}</div> 
+       <div>Expires {props.expirationMonth}/{props.expirationYear}</div>
+        <div>{props.bank}</div> 
+        <div>{props.owner}</div>
+
+    </div>
+  )
+}
+
+export default CreditCard
