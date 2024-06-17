cea8bffc982dcf8ec3759b3fe0b43d22fdef87a2
sai preetham ravula <160527233+VSPR1@users.noreply.github.com>
2024-06-17 23:27:57 +0530

    Create Greetings.js

---
+++ src/Components/Greetings.js
@@ -0,0 +1,25 @@
+mport React from "react";
+
+const Greetings = (props) => {
+
+
+  return (
+    <div className="greet" style={{border:'1px solid black'}}>
+      <p>
+        {" "}
+        {props.lang === "de"
+          ? "Hallo"
+          : props.lang === "en"
+          ? "Hello"
+          : props.lang === "fr"
+          ? "Bonjour"
+          : props.lang === "es"
+          ? "Hii"
+          : ""}{" "}
+        {props.children}
+      </p>
+    </div>
+  );
+};
+
+export default Greetings;
