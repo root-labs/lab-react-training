8b5c16f92152126ba18ccab008d49f469dfd4595
Sreenima05-09-03 <131434989+Sreenima05-09-03@users.noreply.github.com>
2024-09-26 18:58:22 +0530

    Create drivercard.jsx

---
+++ components/drivercard/drivercard.jsx
@@ -0,0 +1,17 @@
+import './DriverCard.css'
+import Rating from '../Rating/Rating';
+function DriverCard(props)
+{
+return <div className='driver'>
+    <div>
+    <img src={props.img} alt='pic' />
+</div>
+<div>
+    <h1>{props.name}</h1>
+    <Rating>{props.rating}</Rating>
+    <p>{props.car.model}-
+    {props.car.licensePlate}</p>
+</div>
+
+</div>
+}
